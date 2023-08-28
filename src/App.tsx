4666398344bfca6c77b09ffd64c6737b1c6454df
Maxim Rudenko <maximu.rudenko@gmail.com>
2023-08-29 00:02:59 +0300

    feat: pasted all components to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import logo from './logo.svg'
 import './App.css'
 
+import { RiEditCircleFill } from 'react-icons/ri'
+
+import { Button } from './Components/Button'
+import { MessageTemplateEditor } from './Components/MessageTemplateEditor'
+import { Modal } from './Components/Modal'
+import templateSource from './Modules/MessageTemplateEditor/fixtures/template.json'
+
 function App() {
+  const [values, setValues] = useState({
+    firstname: 'Bill',
+    lastname: 'Gates',
+    company: 'Bill & Melinda Gates Foundation',
+    position: 'Co-chair',
+  } as Record<string, string>)
+
+  const callbackSave = async (template: string) => {
+    console.log(template)
+  }
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <Modal
+        buttonContent={
+          <Button lg level='primary'>
+            <RiEditCircleFill />
+            Message Editor
+          </Button>
+        }
+        headerContent={
+          <h2 style={{ paddingTop: '2rem', textAlign: 'center', fontSize: '2rem' }}>Message Template Editor</h2>
+        }
+        renderChildren={(setShowModal) => (
+          <div style={{ width: '60vw', height: '70vh', overflow: 'auto', paddingLeft: '4rem', paddingRight: '4rem' }}>
+            <MessageTemplateEditor
+              template={JSON.stringify(templateSource)}
+              values={values}
+              setValues={setValues}
+              callbackSave={callbackSave}
+              setShowModal={setShowModal}
+            />
+          </div>
+        )}
+      ></Modal>
     </div>
   )
 }
