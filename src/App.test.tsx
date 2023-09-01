bbdb37447c41943d4ef336876afe7efb5d2ec330
Maxim Rudenko <maximu.rudenko@gmail.com>
2023-09-01 14:19:03 +0300

    feat: working with data in App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,8 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const button = screen.getByText(/message editor/i)
+  const button = screen.getByText(/^message editor$/i)
+  const buttonWithTestData = screen.getByText(/message editor with test data/i)
   expect(button).toBeInTheDocument()
+  expect(buttonWithTestData).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,85 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './App.css'
 
 import { RiEditCircleFill } from 'react-icons/ri'
 
+import styles from './App.module.css'
 import { Button } from './Components/Button'
+import { Loading } from './Components/Loading'
 import { MessageTemplateEditor } from './Components/MessageTemplateEditor'
 import { Modal } from './Components/Modal'
 import templateSource from './Modules/MessageTemplateEditor/fixtures/template.json'
+import templateRepository from './repository'
+
+const defaultArrVarNames = ['firstname', 'lastname', 'company', 'position']
 
 function App() {
-  const [values, setValues] = useState({
+  const testValues = {
     firstname: 'Bill',
     lastname: 'Gates',
     company: 'Bill & Melinda Gates Foundation',
     position: 'Co-chair',
-  } as Record<string, string>)
+  }
+
+  const [values, setValues] = useState({} as Record<string, string>)
+  const [template, setTemplate] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const callbackSave = async (template: string) => {
-    console.log(template)
+    return templateRepository.setTemplate(template)
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const arrVarNames = await templateRepository.getArrVarNames()
+      if (arrVarNames) {
+        setValues(arrVarNames.reduce((acc, val) => ({ ...acc, [val]: '' }), {}))
+      } else {
+        setValues(defaultArrVarNames.reduce((acc, val) => ({ ...acc, [val]: '' }), {}))
+      }
+      const template = await templateRepository.getTemplate()
+      if (template) {
+        setTemplate(template)
+      }
+      // waiting test loading effect
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className='App'>
+      {loading && (
+        <div className={`${styles.loading}`}>
+          <Loading lg />
+        </div>
+      )}
+      <Modal
+        buttonContent={
+          <Button lg level='primary'>
+            <RiEditCircleFill />
+            Message Editor with test data
+          </Button>
+        }
+        headerContent={
+          <h2 style={{ paddingTop: '2rem', textAlign: 'center', fontSize: '2rem' }}>
+            Message Template Editor with test data
+          </h2>
+        }
+        renderChildren={(closeModal) => (
+          <div style={{ width: '60vw', height: '70vh', overflow: 'auto', paddingLeft: '4rem', paddingRight: '4rem' }}>
+            <MessageTemplateEditor
+              template={JSON.stringify(templateSource)}
+              values={testValues}
+              setValues={setValues}
+              callbackSave={callbackSave}
+              closeModal={closeModal}
+            />
+          </div>
+        )}
+      ></Modal>
       <Modal
         buttonContent={
           <Button lg level='primary'>
@@ -36,7 +93,7 @@ function App() {
         renderChildren={(closeModal) => (
           <div style={{ width: '60vw', height: '70vh', overflow: 'auto', paddingLeft: '4rem', paddingRight: '4rem' }}>
             <MessageTemplateEditor
-              template={JSON.stringify(templateSource)}
+              template={template}
               values={values}
               setValues={setValues}
               callbackSave={callbackSave}
